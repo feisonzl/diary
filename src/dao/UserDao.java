e6f1f4c288d5d362571554dbe9cc9da40c84ecff
feison <feison@DESKTOP-59DGG44>
2017-04-10 01:42:45 +0800

    update UserDao.java

---
+++ src/dao/UserDao.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class UserDao {
+
+	public String register(String sql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -8,13 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDao;
+
 public class UserServlet extends HttpServlet{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private UserDao userDao=null;
+	public UserServlet() {
+		// TODO Auto-generated constructor stub
+		userDao=new UserDao();
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -27,7 +33,10 @@ public class UserServlet extends HttpServlet{
 		String action=req.getParameter("action");
 		if("checkUser".equals(action)){
 			checkUser(req,resp);
+			System.out.println("checkUser!");
 		}else if("register".equals(action)) {
+			System.out.println("register!");	
+			
 			register(req,resp);
 		}
 	}
@@ -38,9 +47,20 @@ public class UserServlet extends HttpServlet{
 		
 	}
 	public void register(HttpServletRequest req,HttpServletResponse resp) throws IOException{
+		String username=req.getParameter("username");
+		String pwd=req.getParameter("pwd");
+		String sql="INSERT INTO tb_user (username,pwd,email,question,answer,city) VALUE ('"
+			+ username
+			+ "','"
+			+ pwd
+			+ "','"+"')";
+		String result = userDao.register(sql);
+		
 		resp.setContentType("text/html");
 		PrintWriter pw= resp.getWriter();
-		pw.print("register success!!");
+		pw.print("register result:"+result+"!!");
+		pw.flush();
+		pw.close();
 	}
 	
 
